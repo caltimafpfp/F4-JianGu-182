d9466b029106b969a9d072a00c29d4f72f10b763
caltimafpfp <b0923721012@gmail.com>
2026-09-15 02:44:20 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// F4撿骨182 Firebase 設定
+window.F4_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyArmva0-pNorWVmE6pdGYHBzW8ZtyxlmZs",
+  authDomain: "f4-jiangu-182.firebaseapp.com",
+  projectId: "f4-jiangu-182",
+  storageBucket: "f4-jiangu-182.firebasestorage.app",
+  messagingSenderId: "275881205977",
+  appId: "1:275881205977:web:8ac39b1955d919722f5ec8"
+};
